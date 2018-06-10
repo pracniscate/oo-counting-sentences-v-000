09afebc14f7960b541e55d63b42fe2fe76b5a2b2
Lorena <lorena.giddens@outlook.com>
2018-06-10 15:33:10 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -12,15 +12,11 @@ class String
   end
 
   def question?
-    if self.end_with?("?")
-      true
-    else
-      false
-    end
+    self.end_with?("?") ? true : false 
   end
 
   def exclamation?
-
+    self.end_with?("?") ? true : false
   end
 
   def count_sentences
