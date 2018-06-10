23f1900d8dfc587ed834105b3a1ff1de52fc52b0
Lorena <lorena.giddens@outlook.com>
2018-06-10 15:32:12 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,12 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-      true
-    else
-      false
-    end
+    # if self.end_with?(".")
+    #   true
+    # else
+    #   false
+    # end
+    self.end_with?(".") ? true : false 
   end
 
   def question?
